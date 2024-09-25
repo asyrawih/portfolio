678bc62032135b92c3eaa879fb74e1582935f6be
asyrawi <hasyrawi@gmail.com>
2024-09-25 17:16:13 +0700

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { Experience } from "@/components/features/experience";
 import { Hero } from "@/components/features/hero";
-
+import Head from "next/head"
 export default function Home() {
   return (
     <>
+    <Head>
+    <meta name="google-site-verification" content="9J8OuOYDw4OBnvBMCDnTYXnn2pECJ_oytIMKxtdpNFI" />
+    </Head>
       <Hero />
       <Experience />
     </>
